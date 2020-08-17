796c98b52416976fed5fb08771cf1d2fa21df3c0
Nguyen Le <nle20959@gmail.com>
2020-08-18 02:29:15 +0700

    Final

---
+++ src/pages/Aboutme.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../App.css";
+
+function Aboutme() {
+  return (
+    <div>
+        
+        
+        <footer class="site-footer">
+<div class="container">
+<div class="row mb-5">
+<p class="col-12 text-center">
+
+Copyright &copy; <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="0e2fadb5967ac6b11fba4ef1-text/javascript">document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+
+</p>
+</div>
+<div class="row mb-5">
+<div class="col-md-12 text-center">
+<p>
+<a href="#" class="social-item"><span class="icon-facebook2"></span></a>
+<a href="#" class="social-item"><span class="icon-twitter"></span></a>
+<a href="#" class="social-item"><span class="icon-instagram2"></span></a>
+<a href="#" class="social-item"><span class="icon-linkedin2"></span></a>
+<a href="#" class="social-item"><span class="icon-vimeo"></span></a>
+</p>
+</div>
+</div>
+</div>
+</footer>
+      
+      </div>
+  );
+}
+
+export default Aboutme;
---
+++ src/pages/Navbar.js
@@ -0,0 +1,246 @@
+import React from "react";
+import "../App.css";
+import { Transition } from "react-spring/renderprops";
+import { Carousel } from "react-bootstrap";
+
+import Aboutme from "./Aboutme";
+
+function Navbar() {
+  const items = [
+    { key: 1, text: "H" },
+    { key: 2, text: "i" },
+    { key: 3, text: "," },
+    { key: 4, text: "c" },
+    { key: 5, text: "a" },
+    { key: 6, text: "l" },
+    { key: 7, text: "l" },
+    { key: 8, text: "\xa0 " },
+    { key: 9, text: "m" },
+    { key: 10, text: "e" },
+    { key: 11, text: "\xa0" },
+    { key: 12, text: "C" },
+    { key: 13, text: "h" },
+    { key: 14, text: "l" },
+    { key: 15, text: "o" },
+    { key: 16, text: "e" },
+    { key: 17, text: "\xa0" },
+    { key: 18, text: "L" },
+    { key: 19, text: "e" },
+  ];
+  return (
+    <div
+      // data-spy="scroll"
+      data-target="#pb-navbar"
+      data-offset="200"
+      class="color size-all"
+    >
+      <Carousel className="size-caro">
+        {/* <Carousel.Item>
+          <nav
+            class="navbar navbar-expand-lg site-navbar navbar-light bg-light"
+            id="pb-navbar"
+          >
+            <div class="container">
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarsExample09"
+                aria-controls="navbarsExample09"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-md-center">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link " href="#section-home">
+                      Home
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#section-portfolio">
+                      My Projects
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#section-about">
+                      About
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#section-contact">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </Carousel.Item> */}
+        <Carousel.Item>
+          <section className="my-name size-caro">
+            <Transition
+              config={{ tension: 10, friction: 2, precision: 0.1 }}
+              items={items}
+              unique={true}
+              reset={true}
+              keys={(item) => item.key}
+              from={{ transform: "translate3d(0,-80px,0)" }}
+              enter={{ transform: "translate3d(0,0px,0)" }}
+              leave={{ transform: "translate3d(0,-80px,0)" }}
+              trail={100}
+            >
+              {(item) => (props) => <div style={props}>{item.text}</div>}
+            </Transition>
+            <div></div>
+          </section>
+        </Carousel.Item>
+        <Carousel.Item>
+          <section class="site-section bg-2" id="section-about">
+            <div class="container">
+              <div class="row mb-5 align-items-center">
+                <div class="col-lg-5 pl-lg-5">
+                  <div class="section-heading">
+                    <h2>
+                      <strong>About Me</strong>
+                    </h2>
+                  </div>
+                  <p class="lead">
+                    Separated they live in Bookmarksgrove right at the coast of
+                    the Semantics, a large language ocean.
+                  </p>
+                  <p class="mb-5  ">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary regelialia. It is a paradisematic
+                    country, in which roasted parts of sentences fly into your
+                    mouth.
+                  </p>
+                </div>
+                <div class="col-lg-7 pr-lg-5 mb-5 mb-lg-0">
+                  <img src="/Images/me.jpg" alt="me" class="size-img" />
+                </div>
+              </div>
+            </div>
+          </section>
+        </Carousel.Item>
+        <Carousel.Item>
+          <section class="site-section size-caro bg-3" id="section-portfolio">
+            <div class="container">
+              <div class="row">
+                <div class="section-heading text-center col-md-12">
+                  <h2>
+                    <strong>My Projects</strong>
+                  </h2>
+                </div>
+              </div>
+              <div class="filters-content">
+                <div class="row grid">
+                  <div class="single-portfolio col-sm-4 all packaging">
+                    <div class="relative">
+                      <div class="thumb">
+                        <a
+                          href="https://amazing-lovelace-9275c8.netlify.app/"
+                          target="_blank"
+                        >
+                          <img
+                            class="image img-fluid"
+                            src="/Images/Tictac.png"
+                            alt=""
+                          />
+                        </a>
+                      </div>
+                    </div>
+                    <div class="p-inner">
+                      <h4>
+                        <strong>Tic Tac Toe</strong>
+                      </h4>
+                    </div>
+                  </div>
+                  <div class="single-portfolio col-sm-4 all packaging">
+                    <div class="relative">
+                      <div class="thumb">
+                        <a
+                          href="https://trusting-archimedes-fc5270.netlify.app/"
+                          target="_blank"
+                        >
+                          <img
+                            class="image img-fluid"
+                            src="/Images/Doremon.png"
+                            alt=""
+                          />
+                        </a>
+                      </div>
+                    </div>
+                    <div class="p-inner">
+                      <h4>
+                        <strong>Hungry Up</strong>
+                      </h4>
+                    </div>
+                  </div>
+                  <div class="single-portfolio col-sm-4 all packaging">
+                    <div class="relative">
+                      <div class="thumb">
+                        <a
+                          href="https://hardcore-wright-d0a0f5.netlify.app/"
+                          target="_blank"
+                        >
+                          <img
+                            class="image img-fluid"
+                            src="/Images/Guessing.png"
+                            alt=""
+                          />
+                        </a>
+                      </div>
+                    </div>
+                    <div class="p-inner">
+                      <h4>
+                        <strong>Guessing Game</strong>
+                      </h4>
+                      r
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </Carousel.Item>
+        <Carousel.Item>
+          <section class="site-section bg-4" id="section-contact">
+            <div class="container">
+              <div class="row position-contact">
+                <div class="col-md-12 mb-5">
+                  <div class="section-heading text-center">
+                    <h2>
+                      <strong>Contact</strong>
+                    </h2>
+                  </div>
+                </div>
+                <div class="position-contact">
+                  <ul class="site-contact-details">
+                    <li>
+                      <span class="text-uppercase">Email</span>
+                      nle20959@gmail.com
+                    </li>
+                    <li>
+                      <span class="text-uppercase">Phone</span>
+                      +84 833 225 280
+                    </li>
+                    <li>
+                      <span class="text-uppercase">Address</span>
+                      Doan Van Bo <br />
+                      Ho Chi Minh
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </section>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Navbar;
